a10b0801fbe7438be5de4cadb758efe8f316f7f1
kevin <kevin@KEVINS-PC>
2025-04-16 14:50:51 -0500

    Adds new files and images

---
+++ Adele Health App/Pages/Journal.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Adele_Health_App.Pages
+{
+    public class JournalModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- Adele Health App/obj/Debug/net8.0/Adele Health App.AssemblyInfo.cs
+++ Adele Health App/obj/Debug/net8.0/Adele Health App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Adele Health App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+819e0efb585f8a8548960f07c4b5a36eb1f80f5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Adele Health App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Adele Health App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
